dbf6a832f7b396eaa9d3f4f9e3b6902ea2396313
BenTay1810 <e1122810@u.nus.edu>
2024-11-18 23:00:47 +0800

    Refactored policy & marketplace contract

--- contracts/InsuranceCompany.sol
+++ contracts/InsuranceCompany.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.20;
 
 import ".deps/npm/@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol"; // for error specification
-import "https://github.com/0x00pluto/solidity-datetime/blob/master/contracts/DateTime.sol"; // for date time
 
 import "./CSToken.sol";
 
@@ -39,6 +38,12 @@ contract InsuranceCompany is Ownable {
         _;
     }
 
+    modifier isListable(uint256 policyId) {
+        Policy storage policy = policies[policyId];
+        require (policy.currPoolValue + policy.minStake <= policy.maxPoolValue, "You cannot list this policy!");
+        _;
+    }
+
     /* Added in creation Time + coverage period to replace active */
     struct Policy {
         uint256 policyId;
@@ -50,25 +55,19 @@ contract InsuranceCompany is Ownable {
         uint256 minStake;
         uint256 creationTime;
         uint coveragePeriod;
-        Date creationDate;     
-        Date coverageEndDate;
         address creator;
         bool listed;
     }
 
-    struct Date {
-        uint256 year;
-        uint256 month;
-        uint256 day;
-    }
-
-
+   
     uint256 public policyCount;
 
     mapping(uint256 => Policy) policies;
 
     event PolicyCreated(uint256 policyId, string policyName, address creator);
     event PolicyClaimed(uint256 policyId, uint256 claimValue); 
+    event PolicyListed(uint256 policyId, string listed);
+    event PolicydeListed(uint256 policyId, string delisted);
 
     function calculateNetClaimValue(Policy memory p)
         public
@@ -84,7 +83,9 @@ contract InsuranceCompany is Ownable {
     function calculateAllNetClaimValue() public view returns (uint256) {
         uint256 sum = 0;
         for (uint256 i = 1; i <= policyCount; i++) {
-            sum += calculateNetClaimValue(policies[i]);
+            if (policies[i].creator == msg.sender) {
+                sum += calculateNetClaimValue(policies[i]);
+            }
         }
         return sum;
     }
@@ -99,34 +100,13 @@ contract InsuranceCompany is Ownable {
         uint _coveragePeriod // default this to days
     ) external checkEnoughTokenBal(_maxPoolValue) validPolicyType(_policyType) {
         require(
-            // I think there is some logic error here on address(this)) being the one to check against
-      
-
-            /* If we are checking based on insurance company contract balance, it will always be 0 at the start,
-               and this condition would always fail
-               cuz only after u create a policy then you would transfer tokens to this contract.
-               At the same time, need to have a mapping to track the max pool value of policies created by specific users.
-
-               Ie. User 1 create policy 1 with 3k tokens, User 2 create policy 2 with 5k tokens,
-                   InsuranceCompany contract would have 8k tokens balance, but it would know 
-                   that how many tokens each user has so based on that then on marketplace side
-                   it will know how to segregate the tokens according to the creators.
-
-            */
-
-            // there needs to be a token transfer to this contract as well ideally (to reduce the max pool from the token owner)
-
+           
             token.balanceOf(address(msg.sender)) > calculateAllNetClaimValue(),
             "Risk of bankruptcy: reduce the max pool value for this policy"
+            // Check against the balance of the msg.sender because he is the one who owns the tokens initially rather than the balance of insuranceCom contract?
         );
 
         policyCount++;
-        Date memory creationDate;
-        (creationDate.year, creationDate.month, creationDate.day) = DateTime.timestampToDate(timestamp);
-        // Call timestampToDate to get year, month, and day and convert to Date struct to prevent stack too deep error
-        uint256 coverageEndTimestamp = timestamp + (_coveragePeriod * 1 days);
-        Date memory coverageEndDate;
-        (coverageEndDate.year, coverageEndDate.month, coverageEndDate.day) = DateTime.timestampToDate(coverageEndTimestamp);
 
 
         policies[policyCount] = Policy({
@@ -135,13 +115,10 @@ contract InsuranceCompany is Ownable {
             policyType: _policyType,
             claimBackRate: _claimBackRate,
             maxPoolValue: _maxPoolValue, 
-            currPoolValue: 0, /* correct me if wrong, curr Pool value should be based on initial max pool value,
-                                 and then decremented as buyers start to purchase the policy? */
+            currPoolValue: 0,
             minStake: _minStake,
             creationTime: block.timestamp,              // track creation time
-            coveragePeriod: _coveragePeriod * 1 days, 
-            creationDate: creationDate,
-            coverageEndDate: coverageEndDate, // think we discussed that time to make it 1 day [prob is blockchain stores as seconds]
+            coveragePeriod: _coveragePeriod * 1 days, //
             creator: msg.sender, 
             listed: false
         });
@@ -154,20 +131,6 @@ contract InsuranceCompany is Ownable {
         return policies[policyId];
     }
 
-    /* A bit lost on needing to add collateral here, i thought adding collateral was to add to no of tokens original owner has?
-       Unless it is to add to the max pool value?  Is this still needed?*/
-    function addCollateral(uint256 amount) external {
-        require(amount > 0, "Amount must be greater than zero");
-        require(
-            token.transferFrom(msg.sender, address(this), amount),
-            "Token transfer failed"
-        );
-
-        // emit TokensBought(msg.sender, amount);
-    }
-
-    /* Does it make sense to have update policy pool? 
-       Usually once the policy is listed, isnt the total insurance coverage/pool fixed? Or are u saying after top up CS tokens, */
     function updatePolicyPoolValue(uint256 policyId, uint256 newPoolValue)
         external
     {
@@ -180,10 +143,25 @@ contract InsuranceCompany is Ownable {
         // can emit an event here
     }
 
+    
+    function _listPolicy(uint256 policyId) public isListable(policyId) {
+        Policy storage policy = policies[policyId];
+        require(policy.listed == false, "Policy is already listed");
+        changePolicyListedStatus(policyId);
+        emit PolicyListed(policyId, "Policy has been listed successfully!");
+    }
+
+    function _deListPolicy(uint256 policyId) public {
+        Policy storage policy = policies[policyId];
+        require(policy.listed == true, "Policy is already delisted");
+        changePolicyListedStatus(policyId);
+        emit PolicydeListed(policyId, "Policy has been delisted successfully!");
+    }
+
     /* Im guessing this should be a private function as previously discussed where we check
        if there is enough tokens left to fulfill the next min token transaction 
        Ie. if curr pool value = 90, min stake = 100, shd delist but remain active */
-    function changePolicyListedStatus(uint256 policyId) external {
+    function changePolicyListedStatus(uint256 policyId) internal {
         Policy storage policy = policies[policyId];
         require(policy.policyId == policyId, "Policy not found");
 
--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -1,19 +1,53 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "./CIToken.sol";
+import "./CSToken.sol";
 import "./InsuranceCompany.sol";
+import "https://github.com/0x00pluto/solidity-datetime/blob/master/contracts/DateTime.sol"; // for date time
+import "@openzeppelin/contracts/utils/Strings.sol"; // for error specification
+
 
 contract Marketplace {
-    CIToken token;
+    CSToken token;
     InsuranceCompany company;
+    uint currentTimestamp = block.timestamp;
 
-    constructor(CIToken _token, InsuranceCompany _company) {
+    constructor(CSToken _token, InsuranceCompany _company) {
         token = _token;
         company = _company;
     }
 
     mapping(address => InsuranceCompany.Policy[]) public policyHolders;
+
+    modifier isActive(uint256 policyId) {
+        InsuranceCompany.Policy memory policy = company.getPolicy(policyId);
+        require(currentTimestamp <= (policy.creationTime + policy.coveragePeriod));
+        _;
+    }
+
+    modifier mustOwnPolicy(uint256 policyId) {
+        InsuranceCompany.Policy memory policy = company.getPolicy(policyId);
+        require(msg.sender == policy.creator, "You do not own this policy");
+        _;
+    }
+
+    struct Date {
+        uint256 year;
+        uint256 month;
+        uint256 day;
+    }
+
+
+
+    function listPolicy(uint256 policyId) public mustOwnPolicy(policyId) {
+        // Date memory creationDate;
+        // (creationDate.year, creationDate.month, creationDate.day) = DateTime.timestampToDate(timestamp);
+        // // Call timestampToDate to get year, month, and day and convert to Date struct to prevent stack too deep error
+        // uint256 coverageEndTimestamp = timestamp + (_coveragePeriod * 1 days);
+        // Date memory coverageEndDate;
+        // (coverageEndDate.year, coverageEndDate.month, coverageEndDate.day) = DateTime.timestampToDate(coverageEndTimestamp);
+        company._listPolicy(policyId);
+    }
 
     function viewListedPolicies()
         external
@@ -47,11 +81,20 @@ contract Marketplace {
     function buyPolicy(uint256 policyId) external payable {
         // Fetch the policy details
         InsuranceCompany.Policy memory policy = company.getPolicy(policyId);
+        require(policy.listed == true, "You cannot buy a policy that has not been listed!");
 
-        uint256 requiredValue = policy.minStake * token.conversionRate();
+        uint256 requiredValue = policy.minStake * token.getUserConversionRate();
         require(
             msg.value >= requiredValue,
-            "You need to pay at least the premium"
+            string(
+                abi.encodePacked(
+                    "You need to pay at least the premium of ", 
+                    Strings.toString(requiredValue), 
+                    " wei, but provided ", 
+                    Strings.toString(msg.value), 
+                    " wei."
+                )
+            )
         );
 
         // Ensure that the policy has enough capacity for more buyers
@@ -60,19 +103,23 @@ contract Marketplace {
             "Policy capacity exceeded"
         );
 
-        if (policy.maxPoolValue - policy.currPoolValue < policy.minStake) {
-            _delistPolicy(policyId);
-        }
-
+       
+        // These are people who have bought policies
         policyHolders[msg.sender].push(policy);
 
         company.updatePolicyPoolValue(
             policyId,
             policy.currPoolValue + policy.minStake
         );
+
+         // Auto-delisting after updating policy pool value
+        if (policy.maxPoolValue - policy.currPoolValue < policy.minStake) {
+            delistPolicy(policyId);
+        }
+
     }
 
-    function _delistPolicy(uint256 policyId) private {
-        company.changePolicyListedStatus(policyId);
+    function delistPolicy(uint256 policyId) private {
+        company._deListPolicy(policyId);
     }
 }
